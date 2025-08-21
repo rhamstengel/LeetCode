05fea08532de9c3dfbf6986364f6cfcea30a4681
rhamstengel <140969658+rhamstengel@users.noreply.github.com>
2025-08-20 19:01:59 -0700

    Time: 19 ms (36.06%), Space: 22.6 MB (25.32%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,12 @@
+from typing import List
+import collections
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
-
+        ans = collections.defaultdict(list)
         for s in strs:
-            # Sort the string to use as a key
-            key = tuple(sorted(s))
-            if key in anagrams:
-                anagrams[key].append(s)
-            else:
-                anagrams[key] = [s]
-
-        return list(anagrams.values())
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return list(ans.values())
