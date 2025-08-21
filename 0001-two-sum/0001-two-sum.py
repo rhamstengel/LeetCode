fe359c23f26eaf536193b43a1661ca7f71c79e1d
rhamstengel <140969658+rhamstengel@users.noreply.github.com>
2025-08-21 11:11:38 -0700

    Time: 0 ms (100%), Space: 18.9 MB (50.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
+        prevMap = {} # val -> index
 
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_to_index:
-                return [num_to_index[complement], i]
-            num_to_index[num] = i
-
-        # Since the problem statement guarantees exactly one solution, 
-        # this line will never be reached if the input is valid.
-        return []
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        
